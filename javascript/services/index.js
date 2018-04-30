0446c68d45b6ee84c13368ee8c648766d82a2a5b
Guilherme Moura Nascimento <gmoura.nas@gmail.com>
2018-04-30 10:42:39 -0300

    created new routes, star and unstar a repo

--- javascript/services/index.js
+++ javascript/services/index.js
@@ -3,7 +3,16 @@ const superagent = require('superagent');
 const GithubServices = {
   getUserInfo () {
     return superagent.get('/user-info');
+  },
+
+  unstar (options) {
+    return  superagent.post('/unstar').send(options);
+  },
+
+  star (options) {
+    return superagent.post('/star').send(options);
   }
+  
 }
 
 module.exports = GithubServices;
